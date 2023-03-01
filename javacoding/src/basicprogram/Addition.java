872f034421457afb1892fd38271f9144ba019601
Admin <Admin@DESKTOP-0LII55N.domain.name>
2023-02-28 22:53:09 -0800

    commit2

---
+++ javacoding/src/basicprogram/Addition.java
@@ -0,0 +1,20 @@
+package basicprogram;
+import java.util.Scanner;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int num1=5;
+		int num2=2;
+		int c;
+		
+	
+     {
+    	 c=num1+num2;
+    	 System.out.println(c);
+     }
+     
+
+	}
+
+}
